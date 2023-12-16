675307473885f463c4b2e21c5757117fe7718e75
GFORJ1973 <gustavoflores.inf@gmail.com>
2023-12-16 11:13:24 -0300

    Implementação

---
+++ producer/src/main/java/br/com/hgl/kafka/producer/resource/MensagemResource.java
@@ -0,0 +1,20 @@
+package br.com.hgl.kafka.producer.resource;
+
+
+import br.com.hgl.kafka.producer.service.MensagemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/mensagem")
+public class MensagemResource {
+    @Autowired
+    MensagemService mensagemService;
+
+    @PostMapping
+    public ResponseEntity<String> enviarMensagem(@RequestBody String mensagem){
+        mensagemService.sendMessage(mensagem);
+        return ResponseEntity.ok().body("Mensagem enviada com sucesso: " + mensagem);
+    }
+}
